3e996e48e5d19cf13b6d08c38729433cc7a75503
Larry Siden <lsiden@gmail.com>
2014-11-19 15:51:59 -0500

    Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -729,4 +729,23 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
+  copy: {
+      dist: {
+          files: [{
+              expand: true,
+              dest: '<%= yeoman.dist %>',
+              cwd: 'heroku',
+              src: '*',
+              rename: function (dest, src) {
+                  var path = require('path');
+                  if (src === 'distpackage.json') {
+                      return path.join(dest, 'package.json');
+                  }
+                  return path.join(dest, src);
+              }
+          }]
+      }
+  }
+
 };
